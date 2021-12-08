6e42eef334fd765b86746554a11640e0a1707ea3
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2021-12-08 11:40:08 +0800

    test: Authing Guard component in vue3&webpack

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,73 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <!--<WrapperedAuthingGuard />-->
+    <AuthingGuard :appId="appId" @login="onLogin" :config="authingConfig" />
+    <!--<HelloWorld :appId="appId" @login="onLogin" :config="authingConfig" />-->
+    <!--<Abc />-->
+    <TestComponent />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import { AuthingGuard } from '@authing/vue-ui-components'
+import { AuthingGuard } from '@authing/vue-ui-components/src/components/index.js'
+import '@authing/vue-ui-components/lib/index.min.css'
+import { h } from 'vue'
+
+console.log('AuthingGuard: ', AuthingGuard)
+
+const TestComponent = {
+  render () {
+    return h('div', {}, this.c)
+  },
+  data () {
+    return {
+      a: 1,
+      b: 2
+    }
+  },
+  computed: {
+    c() {
+      return this.a + this.b
+    }
+  },
+  created () {
+    this._self = {
+      _c: h
+    }
+  },
+  mounted () {
+    console.log('ccccccccc: ', this.c)
+  }
+}
+
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TestComponent,
+    AuthingGuard,
+    // HelloWorld,
+    // Abc: h('div', {
+    //   background: 'blue'
+    // }, 'hello abc')
+  },
+  data () {
+    return {
+      appId: '61ada53ae69e78d80e31d3e4',
+      authingConfig: {
+        apiHost: 'https://lb68p7-demo.authing.cn'
+      }
+    }
+  },
+  mounted () {
+    console.log('----------: ', this)
+  },
+  methods: {
+    onLogin (userInfo) {
+      console.log('onLogin: ', userInfo)
+    }
   }
 }
 </script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,126 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <div class="hello"></div>
 </template>
 
 <script>
+import {
+  getAuthClient,
+  initAuthClient,
+  AuthingGuard as NativeAuthingGuard,
+  GuardEventsCamelToKebabMap,
+  GuardMode,
+  GuardScenes,
+  LoginMethods,
+  RegisterMethods,
+} from "@authing/native-js-ui-components";
+import "@authing/native-js-ui-components/lib/index.min.css";
+
+const callbackEvent = ["before-login", "before-register"];
+
+export { getAuthClient, initAuthClient, GuardMode, GuardScenes, LoginMethods, RegisterMethods };
+
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
+    msg: String,
+    appId: String,
+    config: {
+      type: Object,
+      required: false,
+      default () {
+        return {}
+      }
+    },
+    tenantId: {
+      type: String,
+      required: false,
+      default () {
+        return ''
+      }
+    },
+    visible: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data () {
+    return {
+      localVisible: false,
+      localConfig: this.config
+    }
+  },
+  watch: {
+    visible: {
+      immediate: true,
+      handler(val) {
+        if (val !== this.localVisible) {
+          this.localVisible = val;
+        }
+      },
+    },
+    localVisible: {
+      handler(val) {
+        if (val !== this.visible) {
+          this.$emit("update:visible", val)
+        }
+
+        if (val) {
+          this.show()
+        } else {
+          this.hide()
+        }
+      },
+    },
+  },
+  mounted () {
+    this.localConfig.mode = this.mode
+    this.localConfig.autoRegister = this.autoRegister ? this.autoRegister : this.localConfig.autoRegister;
+    this.localConfig.isSSO = this.isSSO ? this.isSSO : this.localConfig.isSSO;
+    this.localConfig.clickCloseable = this.clickCloseable ? this.clickCloseable : this.localConfig.clickCloseable;
+    this.localConfig.escCloseable = this.escCloseable ? this.escCloseable : this.localConfig.escCloseable;
+
+    const guard = new NativeAuthingGuard(this.appId, this.localConfig, this.tenantId);
+
+    console.log('guard: ', guard)
+
+    const evts = Object.values(GuardEventsCamelToKebabMap);
+    const kebabToCamelMap = Object.entries(GuardEventsCamelToKebabMap).reduce((acc, [camel, kebab]) => {
+      return Object.assign({}, acc, {
+        [kebab]: camel,
+      });
+    }, {});
+
+    const listeners = evts.reduce((acc, evtName) => {
+      return Object.assign({}, acc, {
+        [evtName]: (...rest) => {
+          if (evtName === "close") {
+            this.localVisible = false;
+          }
+          if (!callbackEvent.includes(evtName)) {
+            this.$emit(evtName, ...rest);
+          } else {
+            const camelEvtName = kebabToCamelMap[evtName];
+
+            if (this[camelEvtName]) {
+              return this[camelEvtName](...rest);
+            }
+            return true;
+          }
+        },
+      });
+    }, {});
+
+    evts.forEach((evtName) => guard.on(evtName, listeners[evtName]));
+
+    if (this.localVisible) {
+      guard.show();
+    }
+    this.$guard = guard;
+  },
+  methods: {
+    hide: function() {
+      this.$guard.hide()
+    }
   }
 }
 </script>
